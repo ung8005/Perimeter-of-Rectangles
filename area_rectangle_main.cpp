6ac5eb2a0dfbe61a8e76bfde739d1caeb2993847
ung8005 <62251927+ung8005@users.noreply.github.com>
2022-03-23 05:24:53 -0700

    Add files via upload

---
+++ area_rectangle_main.cpp
@@ -0,0 +1,55 @@
+//EXAM 1 PRACTICE 
+//**********************************************************************
+// File name:   main.cpp
+// Author:      Ashley Ung 
+// Date:        September 21, 2020
+// Class:       CS 150-01     
+// Assignment:  Exam 1 Practice Area of Rectangle  
+// Purpose:     Area of Rectangle 
+// Hours:       1 
+// Computer OS: MacOS
+//**********************************************************************
+
+#include <iostream> 
+using namespace std; 
+
+int main () {
+	
+	int rectangleLengthOne; 
+	int rectangleLengthTwo; 
+	int rectangleWidthOne; 
+	int rectangleWidthTwo; 
+	int areaOne;
+	int areaTwo;
+	
+	cout << "Please enter the length of rectangle one: ";
+	cin >> rectangleLengthOne; 
+	
+	cout << "Please enter the width of rectangle one: ";
+	cin >> rectangleWidthOne;
+	
+	cout << "Please enter the length of rectangle two: "; 
+	cin >> rectangleLengthTwo; 
+	
+	cout << "Please enter the width of rectangle two: ";
+	cin >> rectangleWidthTwo;
+	
+	areaOne = rectangleLengthOne * rectangleWidthOne; 
+	
+	areaTwo = rectangleLengthTwo * rectangleWidthTwo;
+
+	if (areaOne == areaTwo) {
+		cout << "The rectangles have the same area!";
+	}
+	else {
+		if (areaOne > areaTwo) {
+			cout << "Rectangle one has the greater area!";
+		}
+		else {
+			cout << "Rectangle two has the greater area!";
+		}
+	}
+	
+	return 0;
+	
+}
